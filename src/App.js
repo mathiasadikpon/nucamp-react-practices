4584cb540937b952492015f47c88fa2910047487
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-02-26 10:51:07 -0500

    Add ControlledForm component and update routing in App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import UsingRef from './uncontroled/UsingRef';
 import RandomColorRef from './uncontroled/RandomColorRef';
+import ControlledForm from './controled/MyControledForm';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="ref" element={<UsingRef/>}/>
         <Route path="random" element={<RandomColorRef/>}/>
+        <Route path="inputform" element = {<ControlledForm />}/>
       </Routes>
 
     </div>
--- src/controled/MyControledForm.js
+++ src/controled/MyControledForm.js
@@ -1,12 +1,20 @@
+import { Button } from "bootstrap";
 import { useState } from "react";
 
-const ControlledForm = () =>{
-const [inputValue, updateInputValue] = useState('');
+const ControlledForm = () => {
+  const [inputValue, updateInputValue] = useState("");
 
-return (<form>
-    <input type="text" value={inputValue} onClick={""} />
-</form>);
-
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+    updateInputValue(e.target.value);
+  };
+  return (
+    <form>
+      <input type="text" value={inputValue} onChange={handleChange} />
+      {inputValue && <button type="submit">Submit</button>}
+    </form>
+  );
 };
 
 export default ControlledForm;

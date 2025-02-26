4503f53fd19c21a429a9bd968d9f2a464c62d1f3
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-02-25 22:24:00 -0500

    Fix component name and update UsingRef to handle clicks and display values

--- src/using-ref/UsingRef.js
+++ src/using-ref/UsingRef.js
@@ -1,20 +1,24 @@
 import { useRef, useState } from "react";
-function UsinfRef(){
-    const valueWithRef = useRef('not clicked yet');
-    let valuewithoutRef = 'not clicked yet';
+function UsingfRef() {
+  const valueWithRef = useRef("not clicked yet");
+  let valueWithoutRef = "not clicked yet";
 
-    const[timeClicked, updateClicked] = useState(0);
+  const [timesClicked, updateClicked] = useState(0);
 
-    const hansleClick = () => {
-        valueWithRef.current = "CLICKED!!";
-        valuewithoutRef = "CLICKED!!";
-        
-    }
+  const handleClick = () => {
+    valueWithRef.current = "CLICKED!!";
+    valueWithoutRef = "CLICKED!!";
+    console.log("valueWithRef", valueWithRef.current);
+    console.log("valueWithoutRef", valueWithoutRef);
+    updateClicked(timesClicked + 1);
+  };
 
+  return <div className="App">
+    <div>value with ref: {valueWithRef.current}</div>
+            <div>value without ref: {valueWithoutRef}</div>
+            <div>timesClicked: {timesClicked}</div>
+            <button onClick={() => handleClick()}>click me</button>
+  </div>;
+}
 
-    return(<div>
-
-    </div>);
-};
-
-export default UsinfRef;
+export default UsingfRef;

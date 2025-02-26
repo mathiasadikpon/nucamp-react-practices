a19a557d30c9087a8ae4100ae851ed05466e0344
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-02-26 12:17:48 -0500

    Update index.html title and add Bootstrap CDN; enhance App component with navigation links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
 
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-
-import HomePage from './pages/HomePage';
-import UsingRef from './uncontroled/UsingRef';
-import RandomColorRef from './uncontroled/RandomColorRef';
-import ControlledForm from './controled/MyControledForm';
-import ControlledLoginForm from './controled/ControlledLoginForm';
+import HomePage from "./pages/HomePage";
+import UsingRef from "./uncontroled/UsingRef";
+import RandomColorRef from "./uncontroled/RandomColorRef";
+import ControlledForm from "./controled/MyControledForm";
+import ControlledLoginForm from "./controled/ControlledLoginForm";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="ref" element={<UsingRef/>}/>
-        <Route path="random" element={<RandomColorRef/>}/>
-        <Route path="inputform" element = {<ControlledForm />}/>
-        <Route path="controlledloginform" element = {<ControlledLoginForm />}/>
-      </Routes>
+      <ul class="list-group list-group-horizontal">
+        <li class="list-group-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li class="list-group-item">
+          <Link to="ref">useRef</Link>
+        </li>
+        <li class="list-group-item">
+          <Link to="random">Random</Link>
+        </li>
+        <li class="list-group-item">
+          <Link to="inputform">InputForm</Link>
+        </li>
+        <li class="list-group-item">
+          <Link to="inputform">InputForm</Link>
+        </li>
+        <li class="list-group-item">
+          <Link to="controlledloginform">Log In Form</Link>
+        </li>
+      </ul>
 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="ref" element={<UsingRef />} />
+        <Route path="random" element={<RandomColorRef />} />
+        <Route path="inputform" element={<ControlledForm />} />
+        <Route path="controlledloginform" element={<ControlledLoginForm />} />
+      </Routes>
     </div>
   );
 }

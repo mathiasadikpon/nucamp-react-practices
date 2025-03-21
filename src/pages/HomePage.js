f32f815f1a245f4bc9a4aad9bd6db0a9f6d0ab70
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-03-21 19:28:29 -0400

    Enhance HomePage animation with improved opacity and scaling effects using react-spring

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,10 +2,10 @@ import { useSpring, animated } from "react-spring";
 
 const HomePage = () => {
   const animeStyle = useSpring({
-    from: { color: "blue", opacity: 1 , transform: "rotate(20deg)", transform: "scale(2)"},
-    to: { color: "red", opacity: 1,transform: "rotate(0deg)" ,  transform: "scale(1)"},
+    from: { color: "blue", opacity: 0.3 , transform: "rotate(20deg)", transform: "scale(1)"},
+    to: { color: "red", opacity: 1,transform: "rotate(0deg)" ,  transform: "scale(2)"},
     
-    
+    config: {duration:2000},
     loop: true
   });
   return (

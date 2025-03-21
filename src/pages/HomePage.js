19d702b417a165949b94de27850f89c1a6787786
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-03-21 18:38:03 -0400

    Add animation effect to HomePage using react-spring

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,12 @@
+import { useSpring } from "react-spring";
 
 
 const HomePage = () =>{
+    const animeStyle = useSpring({
+        from:{x:0},
+        to:{x:100},
+        config:{duration: Infinity}
+    })
     return( <h3>Home Page</h3>)
 };
 
